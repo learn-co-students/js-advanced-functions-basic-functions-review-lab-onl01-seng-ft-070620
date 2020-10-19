85ac63a0c38181cc6cd08ea31660863232760ff2
Directrix777 <andrew.j.ribas@gmail.com>
2020-10-19 16:16:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+function saturdayFun(verb = 'roller-skate')
+{
+    return `This Saturday, I want to ${verb}!`
+}
+
+function mondayWork(verb = 'go to the office')
+{
+    return `This Monday, I will ${verb}.`
+}
+
+function wrapAdjective(flair = '*')
+{
+    return function(adj = 'special')
+    {
+        return `You are ${flair + adj + flair}!`
+    }
+}
+
+let Calculator = {
+    add: function(a, b)
+    {
+        return a + b
+    },
+
+    subtract: function(a, b)
+    {
+        return a - b
+    },
+
+    multiply: function(a, b)
+    {
+        return a * b
+    },
+
+    divide: function(a, b)
+    {
+        return a / b
+    }
+
+}
+
+function actionApplyer(int, actions)
+{
+    for(let action of actions)
+    {
+        int = action(int)
+    }
+    return int
+}
