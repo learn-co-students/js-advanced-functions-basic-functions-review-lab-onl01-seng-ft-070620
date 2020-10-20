dcdd327db9e3e82922b3d7195346ff8ff2468d6d
Amanda-Katherine <walker.akw@gmail.com>
2020-10-20 00:35:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string = "*") {
+    return function(adj = "special") {
+        return `You are ${string}${adj}${string}!`
+    }
+    let encouragingPromptFunction = wrapAdjective("!!!")
+}
+
+let Calculator = {
+    add: function(a,b) {
+       return a + b
+    },
+    subtract: function(a,b) {
+        return a - b
+    },
+    multiply: function(a,b) {
+        return a * b
+    },
+    divide: function(a,b) {
+        return a / b
+    }
+}
+
+function actionApplyer(int, arr) {
+    if (arr.length === 0) {
+        return int
+    } else {
+        let mathMadness = ((int * 2) + 1000) % 7
+        return mathMadness
+    }
+}
